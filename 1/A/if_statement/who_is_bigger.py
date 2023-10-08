688728b1bab28724b339ddd89a6c364fcf57f113
suriheddo <142180394+vladbogonlov79@users.noreply.github.com>
2023-10-09 01:05:43 +0300

    outputs a larger number, and if equal (zero)

---
+++ 1/A/if_statement/who_is_bigger.py
@@ -0,0 +1,16 @@
+first_number = int(input())
+second_number = int(input())
+
+if first_number < second_number:
+    print (second_number)
+if second_number < first_number:
+    print(first_number)
+if second_number == first_number:
+    print(0)
+
+
+
+
+
+
+
