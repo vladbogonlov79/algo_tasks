38b648d3088598ae91d057fe47e696cf612dedce
suriheddo <142180394+vladbogonlov79@users.noreply.github.com>
2023-10-09 01:13:48 +0300

    comparing numbers

--- 1/A/if_statement/who_is_bigger.py
+++ 1/A/if_statement/who_is_bigger.py
@@ -2,9 +2,9 @@ first_number = int(input())
 second_number = int(input())
 
 if first_number < second_number:
-    print (second_number)
+    print (2)
 if second_number < first_number:
-    print(first_number)
+    print(1)
 if second_number == first_number:
     print(0)
 
