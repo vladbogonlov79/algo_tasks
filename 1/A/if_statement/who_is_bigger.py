932ceec8e5d4063a4c56e51f0098d4f1e7e1d8e6
suriheddo <142180394+vladbogonlov79@users.noreply.github.com>
2023-10-09 08:25:18 +0300

    fixed errors

--- 1/A/if_statement/who_is_bigger.py
+++ 1/A/if_statement/who_is_bigger.py
@@ -2,10 +2,10 @@ first_number = int(input())
 second_number = int(input())
 
 if first_number < second_number:
-    print (2)
-if second_number < first_number:
+    print(2)
+elif second_number < first_number:
     print(1)
-if second_number == first_number:
+elif second_number == first_number:
     print(0)
 
 
