fabf6d0ebf741b0ee4eb0b0c8dd392b3e84d6e2d
suriheddo <hacer-02@mail.ru>
2023-10-20 20:49:05 +0300

    delete (else)

--- 1/A/loops/factorial.py
+++ 1/A/loops/factorial.py
@@ -3,5 +3,5 @@ factorial = 1
 
 for i in range(1, number + 1):
     factorial *= i
-else:
-    print(factorial)
+
+print(factorial)
--- 1/A/loops/sum_of_squares.py
+++ 1/A/loops/sum_of_squares.py
@@ -4,5 +4,5 @@ sum = 0
 for i in range(1, number + 1):
     i = i ** 2
     sum += i
-else:
-    print(sum)
+
+print(sum)
