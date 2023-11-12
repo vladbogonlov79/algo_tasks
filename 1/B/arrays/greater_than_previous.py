6d2b1a36fdc86a8e44cecdf94a24286ed791bde5
suriheddo <hacer-02@mail.ru>
2023-11-12 13:21:17 +0300

    fixed errors

--- 1/B/arrays/greater_than_previous.py
+++ 1/B/arrays/greater_than_previous.py
@@ -2,9 +2,7 @@ n = int(input())
 lts = list(map(int, input().split()))
 quantity = 0
 
-for i in range(n):
-    if i == 1:
-        continue
+for i in range(1, n):
     if lts[i - 1] < lts[i]:
         quantity += 1
 print(quantity)
