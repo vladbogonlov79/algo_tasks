c8c09e0c68eb26db75438da9f47742dff97f6319
suriheddo <hacer-02@mail.ru>
2023-12-02 23:48:22 +0300

    removed the unnecessary check

--- 1/B/arrays/Is_matrix_symmetric.py
+++ 1/B/arrays/Is_matrix_symmetric.py
@@ -6,7 +6,7 @@ for _ in range(n):
     matrix.append(tmp)
 for i in range(n):
     for j in range(n):
-        if matrix[i][j] != matrix[j][i] and i != j:
+        if matrix[i][j] != matrix[j][i]:
             flag = False
             break
 if flag:
