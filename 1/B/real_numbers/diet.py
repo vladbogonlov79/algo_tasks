e1aed98e9d609b17b357f0e7f15325eed62d0846
suriheddo <142180394+vladbogonlov79@users.noreply.github.com>
2024-01-28 13:51:56 +0300

    calculated the diet for animals

---
+++ 1/B/real_numbers/diet.py
@@ -0,0 +1,17 @@
+X, Y, Z = map(float, input().split())
+N = int(input())
+total_protein = 0
+total_fat = 0
+total_carbohydrate = 0
+eps = 0.00001
+
+for i in range(N):
+    Ai, Bi, Ci, Qi = map(float, input().split())
+    total_protein += Ai * Qi
+    total_fat += Bi * Qi
+    total_carbohydrate += Ci * Qi
+
+if total_protein + eps >= X and total_fat + eps >= Y and total_carbohydrate + eps >= Z:
+    print("YES")
+else:
+    print("NO")
