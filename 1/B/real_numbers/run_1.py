dea83ae3697670b2dd5ae33af5b0938313b99db5
suriheddo <142180394+vladbogonlov79@users.noreply.github.com>
2024-03-27 06:29:13 +0300

    fixed errors

--- 1/B/real_numbers/run_1.py
+++ 1/B/real_numbers/run_1.py
@@ -1,7 +1,9 @@
 ran_kilometers, distance = map(float, input().split())
 days = 1
 eps = 1e-6
+
 while abs(distance - ran_kilometers) > eps:
     ran_kilometers += ran_kilometers * 0.7
     days += 1
+    
 print(days)
