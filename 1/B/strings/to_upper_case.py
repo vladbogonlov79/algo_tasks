7d721a8cd146d1b14a50a38ef77eb0210719252a
suriheddo <142180394+vladbogonlov79@users.noreply.github.com>
2024-04-01 00:48:22 +0300

    fixed errors

--- 1/B/strings/to_upper_case.py
+++ 1/B/strings/to_upper_case.py
@@ -1,2 +1,15 @@
-string = input()
-print(string.upper())
+k = input()
+a = 0
+b = ''
+for i in k:
+    ord(i)
+    if ord(i) >= 97 and ord(i) <= 122:
+        a = ord(i) - 32
+        b += chr(a)
+    else:
+        b += chr(ord(i))
+
+print(b)
+
+
+
