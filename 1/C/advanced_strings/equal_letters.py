5df70f203f18fdee67dc315dd915687697a3c99f
vladbogomolov79 <hacer-02@mail.ru>
2024-07-01 21:34:50 +0300

    solved the problem through a nested loop

--- 1/C/advanced_strings/equal_letters.py
+++ 1/C/advanced_strings/equal_letters.py
@@ -1,9 +1,7 @@
-string = input()
-
-for i in range(len(string)):
-    for j in range(i + 1, len(string)):
-        if string[i] == string[j]:
-            print(string[i])
-
+st = input()
+for i in range(len(st)):
+    for j in range(i + 1, len(st)):
+        if st[i] == st[j]:
+            print(st[i])
 
 
