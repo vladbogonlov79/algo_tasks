3d3a21de20cf54098dc06db0deb2348ff75c6496
vladbogonlov79 <hacer-02@mail.ru>
2025-04-02 20:07:40 +0300

    fixed errors

--- 2/B/dynamic_prog/last_num_fib.py
+++ 2/B/dynamic_prog/last_num_fib.py
@@ -4,4 +4,3 @@ a = [1, 1]
 for i in range(2, n + 1):  
     a += [a[-1] + a[-2]]  
 print(str(a[-1])[-1])
-
--- 2/B/multipliers/simplicity_check.py
+++ 2/B/multipliers/simplicity_check.py
@@ -6,3 +6,4 @@ for i in range(2, int(n ** 0.5 + 1)):
         break
 else:
     print("prime")
+    
