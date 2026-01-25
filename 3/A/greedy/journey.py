24f6700a029c38410ea0ee32977d3b945b1f3edb
Vladislav <sniffer@MacBook-Air-Vladislav.local>
2026-01-25 19:18:15 +0300

    fixed errors

--- 3/A/greedy/journey.py
+++ 3/A/greedy/journey.py
@@ -1,9 +1,30 @@
-dist,l_tank = map(int,input().split())
-stations = int(input())
-way = list()
-for _ in range(stations):
-    a = list(map(int,input().split()))
-    way.append(a)
+#n - расстояние между пунктами, k - литров в баке 
+n, k = map(int, input().split())
 
-print(dist,l_tank)
-print(stations,*way)
+data = list(map(int, input().split()))
+s = data[0]
+
+fuel = k
+pos = 0
+ans = 0 
+
+for i in range(1, s + 1):
+    station = data[i]
+    dist = station - pos
+    if dist > k:
+        print(-1)
+        exit()
+    if dist > fuel:
+        ans += 1 
+        fuel = k
+    fuel -= dist
+    pos = station
+
+dist = n - pos
+if dist > k:
+    print(-1)
+    exit()
+if dist > fuel:
+    ans += 1 
+
+print(ans)
